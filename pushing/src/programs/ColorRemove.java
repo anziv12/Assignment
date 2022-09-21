8b8494bc59b74cc1f6296e74e8e426e5a1a4d3c2
anziv <anziv@LAPTOP-HH5Q2VJ1>
2022-09-21 09:55:27 -0500

    first

---
+++ pushing/src/programs/ColorRemove.java
@@ -0,0 +1,20 @@
+package programs;
+import java.util.*;
+public class ColorRemove 
+{
+	public static void main(String[]args)
+	{
+		List<String> l =new ArrayList<String>();
+		l.add("pink");
+		l.add("white");
+		l.add("black");
+		l.add("blue");
+		l.add("red");
+		Iterator<String>it =l.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next()); 
+	}
+
+}
+}
